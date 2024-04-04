da759761ccddeba39a954211afb5a0889e73e35d
mj-003 <272660@student.pwr.edu.pl>
2024-04-04 23:52:58 +0200

    .

--- lab03/filters.py
+++ lab03/filters.py
@@ -1,16 +1,10 @@
 # 2c
 def get_entries_by_addr(log_entries: list[tuple], addr: str):
-    if addr not in [log[0] for log in log_entries]:
-        print(f"Address: {addr} doesn't exist.")
-        return None
     return [log for log in log_entries if log[0] == addr]
 
 
 # 2d
 def get_entries_by_code(log_entries: list[tuple], code: int):
-    if code not in [log[3] for log in log_entries]:
-        print(f"Code: {code} doesn't exist.")
-        return None
     return [log for log in log_entries if log[3] == code]
 
 
@@ -27,9 +21,6 @@ def get_failed_reads(log_entries: list[tuple], join=False):
 
 # 2f
 def get_entries_by_extension(log_entries: list[tuple], extension: str):
-    if not any(log[2].endswith('.' + extension) for log in log_entries):
-        print(f"No entries with extension: {extension} found.")
-        return []
     return [log for log in log_entries if log[2].endswith('.' + extension)]
 
 
